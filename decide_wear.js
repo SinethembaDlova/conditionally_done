012a9e94d57274ca9fbb998df5eedf636c04bbcb
Sinethemba <dSinethemba@gmail.com>
2016-10-07 14:19:23 +0200

    Playing with condition and assertequal.

--- decide_wear.js
+++ decide_wear.js
@@ -16,13 +16,17 @@ var whatToWear = function(weather)
 }
 
 assert.equal(whatToWear(24), "You can wear a " + wardrobe[0] + ", " + wardrobe[1]);
+assert.equal(whatToWear(20), "You can wear a " + wardrobe[0] + ", " + wardrobe[1]);
 assert.equal(whatToWear(17), "You need to wear " + wardrobe[2] + ", " + wardrobe[3] + ", " + wardrobe[4]);
 
 whatToWear(24);
+whatToWear(20);
 whatToWear(17);
 
 var results = whatToWear(24);
+var results1 = whatToWear(20);
 var results2 = whatToWear(17);
 
 console.log(results);
+console.log(results1);
 console.log(results2);
